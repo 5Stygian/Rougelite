b986cb134f024f0ade449e1f327b4cfb3695dcbe
Henry Graff <rlcrafistohard@gmail.com>
2025-02-21 18:32:15 -0800

    update mainloop

--- mainloop.py
+++ mainloop.py
@@ -1,7 +1,7 @@
 from assets.title import title_1
 from assets.tutorial import tutorial_1, tutorial_2
 
-input_ml = ''
+input_ml, tutorial_input = '', ''
 
 # condenses the printing of boxes
 # cs teacher said keeping the code as condensed as possible was a good thing 
@@ -10,24 +10,48 @@ def print_box(NEW_LINE_COUNT, BOX_TO_PRINT):
         print(f'{BOX_TO_PRINT}')
 
 def mainloop():
-        global input_ml
-        input_ml = ''
+        # ==========================================================
+        # variables
+        # ==========================================================
+        global input_ml, tutorial_input
+        input_ml, tutorial_input = '', ''
+        ts_il = True
 
         # ==========================================================
         # title screen
         # ==========================================================
         print_box(100, title_1)
-        input_ml = input('Input: '.lower().strip())
+
+        while ts_il: # title screen input loop
+                try:
+                        input_ml = input('Input: ').lower().strip()
+                        ts_il = False
+                except:
+                        input_ml = input('Input: ').lower().strip()
+        
+        '''
         if input_ml == "startup":
                 print_box(100, title_1)
                 input_ml = input('Input: '.lower().strip())
         elif not input_ml:
                 input_ml = print('\n' + '(Invalid input)' + input('Input: '.lower().strip()) + '\n')
+        '''
 
         # ==========================================================
         # tutorial
         # ==========================================================
         # finish later | reason: i need time to roleplay on c.ai
+
+        if input_ml == "tutorial" or input_ml == "0":
+                match tutorial_input:
+                        case "1":
+                                print_box(10, tutorial_1)
+                                tutorial_input = input('Input: ').lower().strip()
+                        case "2":
+                                print_box(10, tutorial_2)
+                                tutorial_input = input('Input: ').lower().strip()
+        
+        '''                     
         if input_ml == "tutorial" or input_ml == "0" or tutorial_input == "1":
                 while True:
                         try:    
@@ -48,24 +72,15 @@ def mainloop():
         
         elif tutorial_input == "startup":
                 mainloop()
-
-        '''
-        if input_ml == "tutorial" or input_ml == "0" or tutorial_input == "1":
-                print_box(10, tutorial_1)
-                tutorial_input = input('Input: '.lower().strip())
-        elif tutorial_input == "2":
-                print_box(10, tutorial_2)
-                tutorial_input = input('Input: '.lower().strip())
-        elif tutorial_input == "startup":
-                mainloop()      
-        elif not tutorial_input or not input_ml:
-                tutorial_input = print('\n' + '(Invalid input)' + input('Input: '.lower().strip()) + '\n')
         '''
 
         # ==========================================================
         # play
         #==========================================================
+
+        '''
         if input_ml == "play" or input_ml == "1":
                 pass
         elif not input_ml:
                 input_ml = print('\n' + '(Invalid input)' + input('Input: '.lower().strip()) + '\n')
+        '''
