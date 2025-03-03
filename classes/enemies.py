10512571c758ca00749245e079b2a75a9b01d557
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-03-03 11:25:33 -0800

    Update enemies.py

--- classes/enemies.py
+++ classes/enemies.py
@@ -1,13 +1,13 @@
 import player as pla
 
 class Enemy:
-        def __init__(self, health, health_max, defense_flat, defense_nonflat, attack, name):
+        def __init__(self, name, health, health_max, defense_flat, defense_nonflat, attack, ):
+                self.name            = name
                 self.health          = health
                 self.health_max      = health_max
                 self.defense_flat    = defense_flat
                 self.defense_nonflat = defense_nonflat
                 self.attack          = attack
-                self.name            = name
         
         def attack(self):
                 pla.health = pla.health - (self.attack - (pla.defense_flat * pla.defense_nonflat))
