8090483dd654dde8c2ef9742a0d62a4d95592693
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-03-03 11:25:16 -0800

    Update player.py

--- classes/player.py
+++ classes/player.py
@@ -1,13 +1,13 @@
 import enemies as enm
 
 class Player:
-        def __init__(self, health, health_max, defense_flat, defense_nonflat, attack, name):
+        def __init__(self, name, health, health_max, defense_flat, defense_nonflat, attack, ):
+                self.name            = name
                 self.health          = health
                 self.health_max      = health_max
                 self.defense_flat    = defense_flat
                 self.defense_nonflat = defense_nonflat
                 self.attack          = attack
-                self.name            = name
         
         def attack(self):
                 enm.health = enm.health - (self.attack - (enm.defense_flat * enm.defense_nonflat))
