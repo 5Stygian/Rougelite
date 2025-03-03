725f89e1f385a3e10a3192f300bd51f0d471a222
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-03-03 12:35:48 -0800

    Add inventory attributes

--- classes/player.py
+++ classes/player.py
@@ -1,13 +1,15 @@
 import enemies as enm
 
 class Player:
-        def __init__(self, name, health, health_max, defense_flat, defense_nonflat, attack, ):
+        def __init__(self, name, health, health_max, defense_flat, defense_nonflat, attack, equipped, inventory):
                 self.name            = name
                 self.health          = health
                 self.health_max      = health_max
                 self.defense_flat    = defense_flat
                 self.defense_nonflat = defense_nonflat
                 self.attack          = attack
+                self.equipped        = equipped
+                self.inventory       = inventory
         
         def attack(self):
                 enm.health = enm.health - (self.attack - (enm.defense_flat * enm.defense_nonflat))
