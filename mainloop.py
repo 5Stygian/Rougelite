09b0083aacd55edcdcf7d38b8a8f6ce2395a98c7
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-02 12:16:51 -0700

    Update mainloop.py

--- mainloop.py
+++ mainloop.py
@@ -3,16 +3,6 @@ from assets.tutorial import tutorial_1, tutorial_2
 from classes.entities import Player
 from classes.entities import Enemy
 
-pla = Player(
-        "name", # Player name
-        10,     # Health
-        10,     # Maximum health
-        1,      # Flat defense
-        1,      # Nonflat defense
-        1,      # Attack
-        0
-        )
-
 # condenses the printing of boxes
 # cs teacher said keeping the code as condensed as possible was a good thing 
 def print_box(NEW_LINE_COUNT, BOX_TO_PRINT):
