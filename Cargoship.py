f8bddb0730f994a16ff4bbdb42ec2014408c9a3d
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-02 12:15:59 -0700

    Create class

--- Cargoship.py
+++ Cargoship.py
@@ -1,4 +1,5 @@
-def read_file(PATH, START_LINE, END_LINE):
-        with open(PATH, 'r') as file:
-                for i in range(START_LINE - 1, END_LINE):
-                        print(file.readline())
+class Cargoship:
+        def read_file(PATH, START_LINE, END_LINE):
+                with open(PATH, 'r') as file:
+                        for i in range(START_LINE - 1, END_LINE):
+                                print(file.readline())
