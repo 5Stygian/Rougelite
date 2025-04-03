650a374b323749f3a05a39764d6a2012a1ffa3f3
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-03 10:58:34 -0700

    Add an attack test

--- classes/enemies/debug_enemy.py
+++ classes/enemies/debug_enemy.py
@@ -1,12 +1,16 @@
 from entities import Enemy
+from entities import player
 
 DEBUG_ENEMY = Enemy(
         "DEBUG_ENEMY",
         10,
         10, 
-        1,
+        0,
         1,
         1
 )
 
-print(DEBUG_ENEMY)
+# Attack test
+print(f"HP: {DEBUG_ENEMY.health}/{DEBUG_ENEMY.health_max}")
+player.attack("DEBUG_ENEMY")
+print(f"HP: {DEBUG_ENEMY.health}/{DEBUG_ENEMY.health_max}")
