53b81d0d9e4e7cc6fdff903c120072c985a18824
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-11 13:17:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import mainloop
-
-mainloop.mainloop()
+from assets.title import title_1
+from assets.tutorial import tutorial_1, tutorial_2
+from classes.entities import Player
+from classes.entities import Enemy
+ 
+def mainloop():
+        pass
