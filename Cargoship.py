f3796b74f89b548f0a70d682c915e926c654d387
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-11 13:17:49 -0700

    Update Cargoship.py

--- Cargoship.py
+++ Cargoship.py
@@ -3,6 +3,10 @@ class Cargoship:
                 with open(PATH, 'r') as file:
                         for i in range(START_LINE - 1, END_LINE):
                                 print(file.readline())
+        
+        def print_box(NEW_LINE_COUNT, BOX_TO_PRINT):
+               print('\n' * NEW_LINE_COUNT)
+               print(f'{BOX_TO_PRINT}')
 
         def gate_or(arg1, arg2):
                 if arg1 == True | arg2 == True:
