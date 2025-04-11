8923350fe5b523f1b82a05da0336a9a3b3597993
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-11 14:23:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from assets.title import title_1
 from assets.tutorial import tutorial_1, tutorial_2
 from classes.entities import Player
+from classes.entities import player
 from classes.entities import Enemy
  
 def mainloop():
