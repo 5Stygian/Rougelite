bff112130bb12fe02ed034deba4dfed071b05009
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-11 12:36:05 -0700

    Update Cargoship.py

--- Cargoship.py
+++ Cargoship.py
@@ -3,3 +3,18 @@ class Cargoship:
                 with open(PATH, 'r') as file:
                         for i in range(START_LINE - 1, END_LINE):
                                 print(file.readline())
+
+        def gate_or(arg1, arg2):
+                if arg1 == True | arg2 == True:
+                        return True
+                else:
+                        return False
+
+        def gate_and(arg1, arg2):
+                if arg1 == True & arg2 == True:
+                        return True
+                else:
+                        return False
+
+       def gate_not(arg1):
+                return not(arg1)
